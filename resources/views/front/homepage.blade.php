a1e13d4142ac2108651a452b1b43c60d6e00f7b2
tugranDemirel <demireltugran66@gmail.com>
2021-04-15 14:28:54 +0300

    widgets olusuturulumu

--- resources/views/front/homepage.blade.php
+++ resources/views/front/homepage.blade.php
@@ -65,19 +65,6 @@
                 <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
             </div>
         </div>
-    <div class="col-md-3">
-        <div class="card">
-            <div class="card-header">
-                Kategoriler
-            </div>
-            <div class="card-body">
-                <div class="list-group">
-                    @foreach($categories as $category)
-                        <li  class="list-group-item"><a href="">{{$category->name}}</a><span class="badge bg-success float-right">{{$category->count()}}</span></li>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-    </div>
+@include('front.widgets.categoryWidget')
 @endsection
 
