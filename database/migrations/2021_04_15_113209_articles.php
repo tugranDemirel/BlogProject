aae7e3ea7ef493ff159b3ecd4bcf2eb33888265e
tugranDemirel <demireltugran66@gmail.com>
2021-04-15 14:41:00 +0300

    blog yazı tablosu oluşturulması

---
+++ database/migrations/2021_04_15_113209_articles.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Articles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('category_id');
+            $table->string('title');
+            $table->string('image');
+            $table->longText('content');
+            $table->bigInteger('hit')->default(0);
+            $table->string('slug');
+            $table->timestamps();
+
+            // categories tablosu ile article tablosunu birbirine bagladık
+            // onDelete kısmı ilgili kategoriye ait ilgili yazıları da siler onDelete('cascade')
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
